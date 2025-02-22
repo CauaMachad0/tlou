1224333b0d2e8172c7dd748f10d169696606d219
CauaMachad0 <caua.marcelo.machado@outlook.com>
2025-02-21 22:47:05 -0300

    Estilizando o Tlou

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={[styles.baseText, styles.title]}>The last of Us!</Text>
       <Text style={[styles.baseText, styles.info]}>
-        <Text style={styles.label}>Data de lançamento: </Text>14/06/2013</Text>  
+        <Text style={styles.label}>Data de lançamento: </Text>14/06/2013</Text>
+        <Image source={{ uri: 'https://img.olhardigital.com.br/wp-content/uploads/2021/03/Arte-promociomal-do-primeiro-jogo-de-The-Last-of-Us-scaled.jpg' }}
+        style={{width: 300, height: 500}}/>
       <StatusBar style="light" />
     </View>
   );
